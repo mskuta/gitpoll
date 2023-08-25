3d3dbc1c5f4cae329073d2b9e34d4c57b5354b07
Martin Skuta <8618730+mskuta@users.noreply.github.com>
2023-08-25 16:43:06 +0200

    Format source
    The formatter used was Black 23.7.0.

--- gitpoll.py
+++ gitpoll.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # Copyright 2015 Cloudbase Solutions Srl
 # All Rights Reserved.
 #
@@ -24,7 +23,7 @@ import yaml
 
 def get_remote_git_ref(remote_url, branch="master"):
     g = git.Git(".")
-    ref_info = g.ls_remote(remote_url, "refs/heads/" + branch).split('\t')
+    ref_info = g.ls_remote(remote_url, "refs/heads/" + branch).split("\t")
     if ref_info[0]:
         return ref_info[0]
 
@@ -38,16 +37,17 @@ def check_db(db_path):
         "remote_url varchar(400) NOT NULL, "
         "branch varchar(400) NOT NULL, "
         "last_ref varchar(100) NOT NULL, "
-        "PRIMARY KEY (job_name, remote_url, branch))")
+        "PRIMARY KEY (job_name, remote_url, branch))"
+    )
 
 
 def get_last_git_ref(db_path, job_name, remote_url, branch):
     conn = sqlite3.connect(db_path)
     cur = conn.cursor()
     cur.execute(
-        "SELECT last_ref FROM jobs "
-        "WHERE job_name=? AND remote_url=? AND branch=?",
-        (job_name, remote_url, branch))
+        "SELECT last_ref FROM jobs WHERE job_name=? AND remote_url=? AND branch=?",
+        (job_name, remote_url, branch),
+    )
     row = cur.fetchone()
     if row:
         return row[0]
@@ -57,76 +57,76 @@ def set_last_git_ref(db_path, job_name, remote_url, branch, curr_ref):
     conn = sqlite3.connect(db_path)
     cur = conn.cursor()
     cur.execute(
-        "UPDATE jobs set last_ref=? "
-        "WHERE job_name=? AND remote_url=? AND branch=?",
-        (curr_ref, job_name, remote_url, branch))
+        "UPDATE jobs set last_ref=? WHERE job_name=? AND remote_url=? AND branch=?",
+        (curr_ref, job_name, remote_url, branch),
+    )
     if not cur.rowcount:
         cur.execute(
             "INSERT INTO jobs (job_name, remote_url, branch, last_ref) "
             "VALUES (?, ?, ?, ?)",
-            (job_name, remote_url, branch, curr_ref))
+            (job_name, remote_url, branch, curr_ref),
+        )
     conn.commit()
 
 
 def exec_action_url(action_url):
-    print "Executing action: %s" % action_url
+    print("Executing action: %s" % action_url)
     response = requests.get(action_url)
     response.raise_for_status()
 
 
 def process_job(db_path, job_name, job_config):
     run_action = True
-    action_url = job_config.get('action_url')
+    action_url = job_config.get("action_url")
     if not action_url:
         raise ValueError("action_url is required for job %s" % job_name)
 
-    for repo in job_config.get('repos', []):
-        remote_url = repo.get('remote_url')
+    for repo in job_config.get("repos", []):
+        remote_url = repo.get("remote_url")
         if not remote_url:
             raise ValueError("remote_url is required for a repository")
-        branch = repo.get('branch', 'master')
+        branch = repo.get("branch", "master")
 
         curr_ref = get_remote_git_ref(remote_url, branch)
         if not curr_ref:
             raise Exception(
                 "Cannot find a ref for git repo %(remote_url)s, "
-                "branch %(branch)s, make sure the branch exists" %
-                {"remote_url": remote_url, "branch": branch})
+                "branch %(branch)s, make sure the branch exists"
+                % {"remote_url": remote_url, "branch": branch}
+            )
 
-        previous_ref = get_last_git_ref(
-            db_path, job_name, remote_url, branch)
+        previous_ref = get_last_git_ref(db_path, job_name, remote_url, branch)
 
-        print "Repo url: %s" % remote_url
-        print "Curr ref: %s" % curr_ref
-        print "Previous ref: %s" % previous_ref
+        print("Repo url: %s" % remote_url)
+        print("Curr ref: %s" % curr_ref)
+        print("Previous ref: %s" % previous_ref)
 
         if curr_ref != previous_ref:
             if run_action:
                 exec_action_url(action_url)
                 run_action = False
-            set_last_git_ref(
-                db_path, job_name, remote_url, branch, curr_ref)
+            set_last_git_ref(db_path, job_name, remote_url, branch, curr_ref)
 
 
 def main():
     if len(sys.argv) != 2:
-        print "Usage: %s <config.yaml>" % sys.argv[0]
+        print("Usage: %s <config.yaml>" % sys.argv[0])
         return
 
     config_file = sys.argv[1]
-    with open(config_file, 'rb') as s:
+    with open(config_file, "rb") as s:
         config = yaml.load(s)
 
     db_path = config.get("config", {}).get("db", "gitpoll.s3db")
     check_db(db_path)
 
-    jobs = config.get('jobs', {})
+    jobs = config.get("jobs", {})
     for job_name in jobs:
         try:
             process_job(db_path, job_name, jobs[job_name])
         except Exception as ex:
-            print "job failed: %s" % job_name
-            print ex
+            print("job failed: %s" % job_name)
+            print(ex)
 
 
 if __name__ == "__main__":
